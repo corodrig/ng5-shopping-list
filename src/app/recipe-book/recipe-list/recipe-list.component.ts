1fd10e7f4f0272baa2368ac9f4176929491292ad
Christopher Rodriguez <christopher.rodriguez2@ge.com>
2018-02-06 11:31:58 -0600

    make recipe class and recipe list markup

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Mangú', 'Dominican mashed plantains', 'https://media4.s-nbcnews.com/i/newscms/2015_18/511891/mangu-stock-today-150427_a9b0b7fcdf545f4934e89b909314e61d.jpg'),
+    new Recipe('Sancocho', 'Dominican meat stew', 'https://www.dominicancooking.com/wp-content/uploads/2013/05/vegan-sancocho-roots-stew-5.jpg')
+  ];
 
   constructor() { }
 
