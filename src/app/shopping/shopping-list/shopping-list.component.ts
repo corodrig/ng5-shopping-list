e08dbc3e59d48653e5a34dbd13c136d610ea13b0
Christopher Rodriguez <christopher.rodriguez2@ge.com>
2018-02-06 14:35:23 -0600

    start shopping list/edit components

--- src/app/shopping/shopping-list/shopping-list.component.ts
+++ src/app/shopping/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Plantains', 3),
+    new Ingredient('Butter', 1),
+    new Ingredient('Red Onion', 0.5)
+  ];
 
   constructor() { }
 
