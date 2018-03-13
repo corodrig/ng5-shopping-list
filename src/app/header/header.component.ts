42fe1cf21be97d05851ebee05aa743e061508346
Christopher Rodriguez <christopher.rodriguez2@ge.com>
2018-03-13 09:13:31 -0500

    add temp hacky navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, Output, EventEmitter } from "@angular/core";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    @Output()
+    selectedFeature = new EventEmitter<string>();
 
+    onSelect($event) {
+        this.selectedFeature.emit($event);
+    }
 }
